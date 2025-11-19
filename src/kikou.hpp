4b7004f7fbbb643d6b5c0a459a187816cfd0a8b6
Ryouta-pro <r.p2bakxjm@gmail.com>
2025-11-19 15:48:04 +0900

    first commit

---
+++ src/kikou.hpp
@@ -0,0 +1,89 @@
+#include "mbed.h"
+#include <iostream>
+CAN can1(PA_11, PA_12, 1e6);
+CANMessage msg;
+int pwm_data[3] = {20000, 20000, 20000};
+const float k = 360.0 / (1024.0);
+float i_control = 0;
+int before_error = 0;
+float dt = 0.04; // 制御周期
+float p_gain = 1.0;
+float i_gain = 0.0;
+float d_gain = 0.0;
+
+// リミットスイッチ読み取り関数
+void read_limit(bool (&is_sw_push)[5])
+{
+    if (can1.read(msg); msg.id == 10)
+    {
+        uint8_t sw = msg.data[5]; // スイッチの値の内容
+        for (int i = 0; i < 5; i++)
+        {
+            is_sw_push[i] = (sw >> i) & 0x01;
+            // シフトで判別する位を最下位ビットへ移動し、1と論理積を行うことで各位の値を判別する。
+        }
+    }
+}
+
+// エンコーダー読み取り関数
+void read_encoder(int16_t (&enc)[5])
+{
+    if (can1.read(msg); msg.id == 9)
+    {
+        enc[0] = msg.data[7] << 8 | msg.data[6];
+    }
+    if (can1.read(msg); msg.id == 10)
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            enc[i + 1] = msg.data[(i * 2) + 1] << 8 | msg.data[i * 2];
+        }
+    }
+}
+
+// PWM計算関数
+void pwm_calculation(int16_t(&pwm_value), bool btton1, bool button2, int pwm_array_index)
+{
+    if (btton1 || button2)
+    {
+        pwm_value = pwm_data[pwm_array_index] * (btton1 - button2);
+    }
+    else
+    {
+        pwm_value = 0;
+    }
+}
+void sensor_processing(int16_t(&pwm_value), bool limit, bool up_down)
+{
+    if (limit && ((pwm_value > 0) == up_down))
+    {
+        pwm_value = 0;
+    }
+}
+// センサー処理関数
+// limit: リミットスイッチの状態
+// up_down: 正負の判定(true:正の場合停止)
+// void pid_tick()
+// {
+//     // pid_control のシグネチャに合わせて呼ぶ（グローバル変数を参照）
+//     pid_control(pwm1[2], encoder_value[0], want_k);
+// }
+void pid_control(int16_t(&pwm_value), int16_t encoder_value, int16_t want_angle)
+{
+    int error = want_angle - encoder_value * k;
+    float p_control = error;
+    i_control += error * dt;
+    float d_control = (error - before_error) / dt;
+
+    pwm_value = p_gain * p_control + i_gain * i_control + d_gain * d_control;
+    if (pwm_value > 20000)
+    {
+        pwm_value = 20000;
+    }
+    else if (pwm_value < -20000)
+    {
+        pwm_value = -20000;
+    }
+    // printf("now:%d,output:%d\n", k * encoder_value, pwm_value);
+    before_error = error;
+}
---
+++ src/main.cpp
@@ -0,0 +1,54 @@
+// 今回いる機構
+// ロジャー リミットスイッチ×2  20000 両方向 負→上がる 正→下がる
+// garaggaraリミットスイッチ×1 20000 両方向　負→開く 正→閉じる
+// 今now ロリコン×1 20000　正転
+#include "mbed.h"
+#include <iostream>
+#include "kikou.hpp"
+CAN can(PB_12, PB_13, 1e6);
+BufferedSerial pc(USBTX, USBRX, 115200);
+CANMessage msg1;
+// const float k = 360.0 / (1024.0); // 360 / (512*2)
+bool limit_value[5] = {0};
+int16_t encoder_value[5] = {0};
+int16_t pwm1[4] = {0};
+int16_t want_k = 45;
+bool controller[12] = {false}; // r2,l2,r,l, up, down, left, right, circle, cross, triangle, square
+Ticker pid_starter;
+
+void pid_tick()
+{
+    // pid_control のシグネチャに合わせて呼ぶ（グローバル変数を参照）
+    pid_control(pwm1[2], encoder_value[0], want_k);
+}
+
+int main()
+{
+    pid_starter.attach(&pid_tick, 0.04); // 0.04 秒ごとに pid_tick を呼ぶ
+    while (1)
+    {
+        // リミットスイッチ、エンコーダー読み取り
+        read_limit(limit_value);
+        read_encoder(encoder_value);
+        // コントローラー入力からPWM計算
+        controller[0] = true;
+        controller[2] = true;
+        controller[9] = true;
+        pwm_calculation(pwm1[0], controller[0], controller[1], 0); // カード1,r2,l2,pwm_data配列0番
+        pwm_calculation(pwm1[1], controller[2], controller[3], 1); // カード2,r,l,pwm_data配列1番
+        // pwm_calculation(pwm1[2], controller[8], controller[9], 2); // カード3,circle,cross,pwm_data配列2番
+        // センサー処理
+        sensor_processing(pwm1[0], limit_value[0], 1); // ロジャー下リミット
+        sensor_processing(pwm1[0], limit_value[1], 0); // ロジャー上リミット
+        sensor_processing(pwm1[1], limit_value[2], 1); // garaggaraリミット
+        // 　ロリコン処理
+        // pid_control(pwm1[3], encoder_value[0], want_k);
+        printf("enc:%d,%d,%d,%d,%d\n", encoder_value[0], encoder_value[1], encoder_value[2], encoder_value[3], encoder_value[4]);
+        printf("lim:%d,%d,%d,%d,%d\n", limit_value[0], limit_value[1], limit_value[2], limit_value[3], limit_value[4]);
+        // pwmデータの確認
+        printf("%d,%d,%d,%d\n", pwm1[0], pwm1[1], pwm1[2], pwm1[3]);
+        // モタドラへcan送信
+        CANMessage msg1(2, (const uint8_t *)pwm1, 8);
+        can.write(msg1);
+    }
+}
