59353e5fb611d14716b011d89a153c2d1a8140e1
Ryouta-pro <r.p2bakxjm@gmail.com>
2025-12-15 15:18:15 +0900

    saikyou

--- src/kikou.hpp
+++ src/kikou.hpp
@@ -1,16 +1,9 @@
 #include "mbed.h"
 #include <iostream>
+#include "controler.hpp"
 CAN can1(PB_12, PB_13, 1e6);
 CANMessage msg;
-int pwm_data[3] = {20000, 20000, 20000};
-const float k = 360.0 / (1024.0);
-float i_control = 0;
-int before_error = 0;
-float dt = 0.04; // 制御周期
-float p_gain = 1.0;
-float i_gain = 0.0;
-float d_gain = 0.0;
-
+// CANMessage msg1;
 // リミットスイッチ読み取り関数
 void read_limit(bool (&is_sw_push)[5])
 {
@@ -42,22 +35,48 @@ void read_encoder(int16_t (&enc)[5])
 }
 
 // PWM計算関数
-void pwm_calculation(int16_t(&pwm_value), bool btton1, bool button2, int pwm_array_index)
+void pwm_calculation(int16_t(&pwm_value), bool button1, bool button2, int value)
 {
-    if (btton1 || button2)
-    {
-        pwm_value = pwm_data[pwm_array_index] * (btton1 - button2);
-    }
-    else
-    {
-        pwm_value = 0;
-    }
+    pwm_value = (button1 - button2) * value;
 }
+
 void sensor_processing(int16_t(&pwm_value), bool limit, bool up_down)
 {
     if (limit && ((pwm_value > 0) == up_down))
     {
         pwm_value = 0;
+    }
+}
+void mekanamu(int16_t (&pwm)[4])
+{
+    // メカナム用の足回り計算
+    // printf("%d\n",(int)stick_value["lx"]);
+    float omuni_value = 15000;
+    float power = hypot(stick_value["lx"], stick_value["ly"]);
+    float angle = atan2((-1 * stick_value["ly"]), stick_value["lx"]);
+    int num2 = (int)(6000.0 * stick_value["rx"]);
+
+    for (int i = 0; i < 4; i++)
+    {
+
+        int num = -1 * (int)(sin((M_PI / 180 * (90 * i + 45)) + angle) * power * omuni_value);
+        if (num != 0)
+        {
+            pwm[i] = num;
+        }
+        else
+        {
+            pwm[i] = num2;
+        }
+
+        if (pwm[i] > 15000)
+        {
+            pwm[i] = 0;
+        }
+        else if (pwm[i] < -15000)
+        {
+            pwm[i] = 0;
+        }
     }
 }
 // センサー処理関数
@@ -68,22 +87,28 @@ void sensor_processing(int16_t(&pwm_value), bool limit, bool up_down)
 //     // pid_control のシグネチャに合わせて呼ぶ（グローバル変数を参照）
 //     pid_control(pwm1[2], encoder_value[0], want_k);
 // }
-// void pid_control(int16_t(&pwm_value), int16_t encoder_value, int16_t want_angle)
-// {
-//     int error = want_angle - encoder_value * k;
-//     float p_control = error;
-//     i_control += error * dt;
-//     float d_control = (error - before_error) / dt;
+float i_control = 0;
+int before_error = 0;
+float dt = 0.04; // 制御周期
+float p_gain = 1.0;
+float i_gain = 0.0;
+float d_gain = 0.0;
+void pid_control(int16_t(&pwm_value), int16_t encoder_value, int16_t want_value)
+{
+    int error = want_value - encoder_value;
+    float p_control = error;
+    i_control += error * dt;
+    float d_control = (error - before_error) / dt;
 
-//     pwm_value = p_gain * p_control + i_gain * i_control + d_gain * d_control;
-//     if (pwm_value > 20000)
-//     {
-//         pwm_value = 20000;
-//     }
-//     else if (pwm_value < -20000)
-//     {
-//         pwm_value = -20000;
-//     }
-//     // printf("now:%d,output:%d\n", k * encoder_value, pwm_value);
-//     before_error = error;
-// }
+    pwm_value = p_gain * p_control + i_gain * i_control + d_gain * d_control;
+    if (pwm_value > 10000)
+    {
+        pwm_value = 10000;
+    }
+    else if (pwm_value < -10000)
+    {
+        pwm_value = -10000;
+    }
+    before_error = error;
+    // printf("%d\n", pwm_value);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -2,54 +2,69 @@
 // ロジャー リミットスイッチ×2  20000 両方向 負→上がる 正→下がる 9v
 // garaggaraリミットスイッチ×1 20000 両方向　負→開く 正→閉じる 5v
 // 今now ロリコン×1 20000　正転 9v
-#include "mbed.h"
+
+// ラッピニ r
+// ベルト r
+// バイスト r
+// 滑車 r
+
+// 櫓ラッピニ
+// アーム r
+// ベルト櫓 r
+
+// リング棒
+// リングベルト r r
+
 #include <iostream>
 #include "kikou.hpp"
+#include "FIRSTPENGUIN.hpp"
+#include <cmath>
+
 CAN can(PA_11, PA_12, 1e6);
-BufferedSerial pc(USBTX, USBRX, 115200);
-CANMessage msg1;
+int16_t pwm1[4] = {0};
+int16_t pwm[4] = {0};
+
+constexpr uint32_t can_id = 35;    // FPのCAN通信のIDを設定
+FirstPenguin penguin(can_id, can); // FPの設定
+// BufferedSerial pc(USBTX, USBRX, 115200);
+// BufferedSerial esp(PA_9, PA_10, 115200);
 // const float k = 360.0 / (1024.0); // 360 / (512*2)
 bool limit_value[5] = {0};
 int16_t encoder_value[5] = {0};
-int16_t pwm1[4] = {0};
-int16_t want_k = 45;
-bool controller[12] = {false}; // up, down, right, left,triangle,circle, cross,  square,l,r,l2,r2,
+// int pojisyon[3] = {0};
+// bool control1ler[12] = {false}; // up, down, right, left,triangle,circle, cross,  square,l,r,l2,r2,
 // PID制御用Ticker              // up 0, down 1, right 2,left 3,triangle 4,circle 5,cross 6,square 7,l 8,r 9,l2 10,r2 11,
-Ticker pid_starter;
-
-// void pid_tick()
-// {
-//     // pid_control のシグネチャに合わせて呼ぶ（グローバル変数を参照）
-//     pid_control(pwm1[2], encoder_value[0], want_k);
-// }
 
 int main()
 {
-    // pid_starter.attach(&pid_tick, 0.04); // 0.04 秒ごとに pid_tick を呼ぶ
     while (1)
     {
+        // pid_control(pwm1[0],encoder_value[0],10000);
+        // コントローラー入力読み取り
+        read_controller();
+        // controller_check();
         // リミットスイッチ、エンコーダー読み取り
         read_limit(limit_value);
         read_encoder(encoder_value);
         // コントローラー入力からPWM計算
-        controller[0] = true;
-        controller[2] = true;
-        controller[9] = true;
-        pwm_calculation(pwm1[0], controller[10], controller[11], 0); // カード1,l2,r2,pwm_data配列0番
-        pwm_calculation(pwm1[1], controller[8], controller[9], 1);   // カード2,l,r,pwm_data配列1番
-        pwm_calculation(pwm1[2], controller[4], controller[5], 2);   // カード3,circle,cross,pwm_data配列2番
+        pwm_calculation(pwm1[0], controller["L1"], controller["R1"], 15000);  // カード1,l2,r2,pwm_data配列0番
+        pwm_calculation(pwm1[1], controller["ci"], controller["sq"], 5000);   // カード2,l,r,pwm_data配列1番
+        pwm_calculation(pwm1[2], controller["tri"], controller["cr"], 15000); // カード3,circle,cross,pwm_data配列2番
         // センサー処理
         sensor_processing(pwm1[0], limit_value[0], 1); // ロジャー下リミット
         sensor_processing(pwm1[0], limit_value[1], 0); // ロジャー上リミット
         sensor_processing(pwm1[1], limit_value[2], 1); // garaggaraリミット
         // 　ロリコン処理
-        // pid_control(pwm1[3], encoder_value[0], want_k);
+        // pid_control(pwm1[3], encoder_value[0], 10000);
         // printf("enc:%d,%d,%d,%d,%d\n", encoder_value[0], encoder_value[1], encoder_value[2], encoder_value[3], encoder_value[4]);
         // printf("lim:%d,%d,%d,%d,%d\n", limit_value[0], limit_value[1], limit_value[2], limit_value[3], limit_value[4]);
-        // pwmデータの確認
-        printf("%d,%d,%d,%d\n", pwm1[0], pwm1[1], pwm1[2], pwm1[3]);
+        // printf("%d,%d,%d,%d\n", (int)(stick_value["lx"] * 100), (int)(stick_value["ly"] * 100), (int)(stick_value["rx"] * 100), (int)(stick_value["ry"] * 100));
+        mekanamu(penguin.pwm);
         // モタドラへcan送信
-        CANMessage msg1(2, (const uint8_t *)pwm1, 8);
-        can.write(msg1);
+        penguin.send();
+        CANMessage msg(2, (const uint8_t *)pwm1, 8);
+        // CANMessage msg1(4, (const uint8_t *)pwm, 8);
+        can.write(msg);
+        // can.write(msg1);
     }
 }
